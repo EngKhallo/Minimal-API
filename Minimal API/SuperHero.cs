772e2054b12d94d57f04770df8ea2c0998b1dc85
EngKhallo <engkhalid.ict@gmail.com>
2022-05-21 14:52:16 +0300

    SuperHero Model

--- Minimal API/SuperHero.cs
+++ Minimal API/SuperHero.cs
@@ -3,7 +3,8 @@
     public class SuperHero
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string? FirstName { get; set; }
+        public string LastName { get; set; }
         public string? HeroName { get; set; }
     }
 }
