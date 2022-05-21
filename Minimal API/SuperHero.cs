41050e587d686ce83c08e62bdfcd0696568f69cc
EngKhallo <engkhalid.ict@gmail.com>
2022-05-21 14:36:29 +0300

    Add project files.

---
+++ Minimal API/SuperHero.cs
@@ -0,0 +1,9 @@
+﻿namespace Minimal_API
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? HeroName { get; set; }
+    }
+}
